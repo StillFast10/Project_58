b9bf43cad593a35eaa24da6e361324660acde94d
v0lt <stillfast43@gmail.com>
2025-10-01 22:28:47 -0500

    Remove target directory and add .gitignore

--- apigateway/target/debug/deps/apigateway-4c4150090e6f66dc.d
+++
@@ -1,5 +0,0 @@
-/home/volt/proyectos/Project_58/apigateway/target/debug/deps/libapigateway-4c4150090e6f66dc.rmeta: src/main.rs
-
-/home/volt/proyectos/Project_58/apigateway/target/debug/deps/apigateway-4c4150090e6f66dc.d: src/main.rs
-
-src/main.rs:
