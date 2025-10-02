9f0afb7507106e85b45c211998d1d69a4341f4f6
v0lt <stillfast43@gmail.com>
2025-10-01 22:20:58 -0500

    Initialize apigateway service with Cargo project

---
+++ apigateway/target/debug/deps/apigateway-4c4150090e6f66dc.d
@@ -0,0 +1,5 @@
+/home/volt/proyectos/Project_58/apigateway/target/debug/deps/libapigateway-4c4150090e6f66dc.rmeta: src/main.rs
+
+/home/volt/proyectos/Project_58/apigateway/target/debug/deps/apigateway-4c4150090e6f66dc.d: src/main.rs
+
+src/main.rs:
---
+++ apigateway/target/debug/deps/apigateway-c7add50a4939fa5b.d
@@ -0,0 +1,5 @@
+/home/volt/proyectos/Project_58/apigateway/target/debug/deps/libapigateway-c7add50a4939fa5b.rmeta: src/main.rs
+
+/home/volt/proyectos/Project_58/apigateway/target/debug/deps/apigateway-c7add50a4939fa5b.d: src/main.rs
+
+src/main.rs:
